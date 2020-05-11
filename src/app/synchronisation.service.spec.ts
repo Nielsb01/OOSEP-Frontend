7a6c6931db6efb5085920a10278d01785ea7fd9c
Bart Kessels <development@bk-mail.com>
2020-05-11 16:00:34 +0200

    Add missing import for sync service test

--- src/app/synchronisation.service.spec.ts
+++ src/app/synchronisation.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SynchronisationService } from './synchronisation.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('SynchronisationServiceService', () => {
   let service: SynchronisationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(SynchronisationService);
   });
 
