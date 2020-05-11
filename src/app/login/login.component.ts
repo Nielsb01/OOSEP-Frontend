74608f1e508721ededd436fe5f6f718f62ce9fb5
Nielsb01 <nielsb1101@gmail.com>
2020-05-11 11:16:27 +0200

    Login forntend html css

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+
+
+import {Component} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  loginForm;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      syncLoginUsername: '',
+      syncLoginPassword: ''
+    });
+  }
+
+  onSubmit(loginData) {
+    console.warn('login attempt made: ', loginData);
+  }
+}
+
